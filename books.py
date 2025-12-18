fca4f644b350e0b7e91716dc6c95e9c725adc3c3
bmakinen <makinenbeda@gmail.com>
2025-12-18 19:28:12 +0200

    Show item list/info

--- books.py
+++ books.py
@@ -4,3 +4,19 @@ def add_book(title, author, review, user_id):
     sql = """INSERT INTO books (title, author, review, user_id)
             VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, author, review, user_id])
+
+
+def get_books():
+    sql = "SELECT id, title, author FROM books ORDER BY id DESC"
+    return db.query(sql)
+
+
+def get_book(book_id):
+    sql = """SELECT books.title,
+                    books.author,
+                    books.review,
+                    users.username
+            FROM books, users
+            WHERE books.user_id = users.id AND
+                  books.id = ?"""
+    return db.query(sql, [book_id])[0]
